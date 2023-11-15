a719fb9c32dd7da371921f195c8b4da7c0d6a4bf
Kontiko <kontiko.fb@gmail.com>
2023-11-15 22:28:05 +0100

    Added real time mapping

--- Scripts/Orbit_control.gd
+++ Scripts/Orbit_control.gd
@@ -17,12 +17,12 @@ var aop = 0.0
 var incl = 0.0
 var aoa = 0.0
 var period = 0
-var time = 1.0
+var time = 0.0
+var last_orbit = 0.0
 var pos = 0
 var pos_offset = 0
 var delta_area = []
 var complete_area = 0.0
-
 var orbit_steps = 0.0
 
 func update_Mesh():
@@ -58,6 +58,7 @@ func update_Mesh():
 	mesh = arr_mesh
 
 func calc_vec(angle: float):
+	angle = fposmod(angle+PI,2*PI)
 	var vec = Vector3(semi_major*sin(angle)/1000.0,0,semi_minor*cos(angle)/1000.0)
 	vec = vec-Vector3((semi_major-periapsis-reference_radius)/1000.0,0,0)
 	vec = vec.rotated(Vector3(0,1,0),aop/180.0*PI)
@@ -76,16 +77,10 @@ func _ready():
 #	pass
 
 
-func _on_TextureButton_toggled(button_pressed):
-	$Control/TextureButton.flip_v = button_pressed
-	$Configs.visible = button_pressed
 
-
-func _on_Button_pressed():
-	emit_signal("delete",name)
-	self.queue_free()
-
-
+################################################################################
+#Sync basic orbit config
+################################################################################
 func set_peri(per):
 	if per < apoapsis:
 		periapsis = per
@@ -134,6 +129,9 @@ func set_minor(minor):
 	apoapsis = semi_major + e*semi_major -reference_radius
 
 
+###############################################################################
+#Button Pressed actions
+###############################################################################
 func _on_apo_in_value_changed(value):
 	if value == (apoapsis):
 		return
@@ -147,15 +145,11 @@ func _on_peri_in_value_changed(value):
 	set_peri(value)
 	changed()
 
-
-
-
 func _on_major_in_value_changed(value):
 	if value == (semi_major):
 		return
 	set_major(value)
 	changed()
-
 
 func _on_minor_in_value_changed(value):
 	if value == semi_minor:
@@ -163,13 +157,45 @@ func _on_minor_in_value_changed(value):
 	set_minor(value)
 	changed()
 
-
 func _on_Ecc_in_value_changed(value):
 	if value == e:
 		return
 	set_e(value)
 	changed()
-	
+
+func _on_ColorPickerButton_color_changed(color_in):
+	color = color_in
+	changed()
+
+func _on_aop_in_value_changed(value):
+	aop = value
+	changed()
+
+func _on_aoa_in_value_changed(value):
+	aoa = value
+	changed()
+	changed()
+
+func _on_Incl_in_value_changed(value):
+	incl = value
+	changed()
+
+func _on_TextureButton_toggled(button_pressed):
+	$Control/TextureButton.flip_v = button_pressed
+	$Configs.visible = button_pressed
+
+func _on_Button_pressed():
+	emit_signal("delete",name)
+	self.queue_free()
+
+func _on_pos_in_value_changed(value):
+	pos = int(value*point_count/360)
+	time = delta_area[pos]/complete_area*period
+	emit_signal("changed",name)
+
+###############################################################################
+#Update  Orbit parameters after one changed
+###############################################################################
 
 func changed():
 	$Configs/apo_in.value = apoapsis
@@ -183,33 +209,31 @@ func changed():
 	emit_signal("changed",name)
 
 
-
-func _on_ColorPickerButton_color_changed(color_in):
-	color = color_in
-	changed()
-
-
-func _on_aop_in_value_changed(value):
-	aop = value
-	changed()
-
-
-func _on_aoa_in_value_changed(value):
-	aoa = value
-	changed()
-
-
-func _on_Incl_in_value_changed(value):
-	incl = value
-	changed()
-
-func update_postion(delta):
-	time += delta
-	if time > period:
-		orbit_steps += period/PlanetInfo.rotation_period*2*PI
+#Function run by Main to update position in real time
+func update_postion(unix):
+	time = unix
+	var changed = false
+	while last_orbit + period < unix:
+		changed = true
+		last_orbit += period
+	while last_orbit - period > unix:
+		changed = true
+		last_orbit -= period
+		#orbit_steps += period/PlanetInfo.rotation_period*2*PI
+	if changed:
 		emit_signal("changed",name)
-	time = fmod(time,period)
-	pos = searchpos(complete_area*float(time)/period)
+	pos = searchpos(complete_area*float(time-last_orbit)/period)
+	$Configs/pos_in.get_line_edit().text = str(pos*360.0/point_count)
+
+func orbit_step():
+	var dt = fposmod(last_orbit-PlanetInfo.solstice_unix_offset,PlanetInfo.period)
+	var angle = 2*PI*dt/PlanetInfo.period
+	var time_dict = Time.get_time_dict_from_unix_time(int(last_orbit))
+	var earth_rot = (float(time_dict.hour)/24.0
+					+float(time_dict.minute)/1440.0
+					+float(time_dict.second)/86400)*2*PI
+	return earth_rot + angle + PI/2
+#Find Current Positional index By Area gone with the helo of binary search
 func searchpos(area):
 	var start = 0
 	var end = len(delta_area)
@@ -223,19 +247,20 @@ func searchpos(area):
 			start = probe_pos
 	return start
 
+#Calculate ground path
 func calc_groundpath():
 	var points_out = []
 	var points_in = points
 	for j in range(2):
 		for i in range(len(points)):
-			var ind_1 = (i + pos_offset)%point_count
-			var o_1 = (i + pos_offset)/point_count
-			var ind_2 = (i + 1 + pos_offset)%point_count
-			var o_2 = (i + 1 + pos_offset)/point_count
+			var ind_1 = (i )%point_count
+			var o_1 = (i )/point_count
+			var ind_2 = (i + 1 )%point_count
+			var o_2 = (i + 1 )/point_count
 			var p1 = to_2d(points[ind_1])
 			var p2 = to_2d(points[ind_2])
-			p1.x = p1.x + PlanetInfo.base_angle - (delta_area[ind_1]/complete_area+j+o_1)*period/PlanetInfo.rotation_period*2*PI - orbit_steps
-			p2.x = p2.x + PlanetInfo.base_angle - (delta_area[ind_2]/complete_area+j+o_2)*period/PlanetInfo.rotation_period*2*PI - orbit_steps
+			p1.x = p1.x + PlanetInfo.base_angle - (delta_area[ind_1]/complete_area+j+o_1)*period/PlanetInfo.rotation_period*2*PI - orbit_step()
+			p2.x = p2.x + PlanetInfo.base_angle - (delta_area[ind_2]/complete_area+j+o_2)*period/PlanetInfo.rotation_period*2*PI - orbit_step()
 			p2.x = fposmod(p2.x,2*PI)
 			p1.x = fposmod(p1.x,2*PI)
 			p1 = p1 * Vector2(1024.0/(2*PI),512.0/(PI))
@@ -249,23 +274,20 @@ func calc_groundpath():
 		low_res.append(points_out[i+point_steps-1])
 	return low_res
 
+#Calculate Lat,Lng, coordinates for Point by Normalizing the Vectors
 func to_2d(point):
 	var point_in  = point.normalized()
 	var lng = fposmod(atan2(point_in.x,point_in.z),2*PI)
 	var lat = -asin(point_in.y)
 	return Vector2(lng,lat)
 
+#Calculate Current Position of sattelite in Lat,Lng Coordinate Space
 func current_pos_2d():
-	var pos_2d = to_2d(points[get_pos()])
-	pos_2d.x += PlanetInfo.base_angle - (delta_area[get_pos()]/complete_area)*period/PlanetInfo.rotation_period*2*PI - orbit_steps
-	pos_2d.x += ((pos+pos_offset)/point_count)*period/PlanetInfo.rotation_period*2*PI
+	var pos_2d = to_2d(points[pos])
+	pos_2d.x += PlanetInfo.base_angle - (delta_area[pos]/complete_area)*period/PlanetInfo.rotation_period*2*PI - orbit_step()
+	pos_2d.x += ((pos)/point_count)*period/PlanetInfo.rotation_period*2*PI
 	pos_2d.x = fposmod(pos_2d.x,2*PI)
 	pos_2d = pos_2d * Vector2(1024.0/(2*PI),512.0/(PI))
 	return pos_2d
 
-func get_pos():
-	return (pos_offset+pos)%point_count
 
-func _on_pos_in_value_changed(value):
-	pos_offset = int(value*point_count/360)
-	
